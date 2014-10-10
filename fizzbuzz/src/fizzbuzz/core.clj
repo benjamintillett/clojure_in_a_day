e17f37bfe7a93ab6b373d21b37054f9677985566
benjamintillett <benjamin.tillett@gmail.com>
2014-10-10 12:02:40 +0100

    add working fizzbuzz

--- fizzbuzz/src/fizzbuzz/core.clj
+++ fizzbuzz/src/fizzbuzz/core.clj
@@ -7,4 +7,23 @@
 
 
 
-(defn divisible-by-three? [number] (zero? (mod number 3) ))
+(defn divisible-by-three? [number] 
+	(zero? (mod number 3))
+)
+
+(defn divisible-by-five? [number]  
+	(zero? (mod number 5))
+)
+
+(defn divisible-by-fifteen? [number] 
+	(and (divisible-by-five? number) (divisible-by-three? number))
+)
+
+(defn game [number] 
+	(if (divisible-by-fifteen? number) "FizzBuzz"
+		(if (divisible-by-three? number) "Fizz"
+			(if (divisible-by-five? number) "Buzz"
+			number)
+		)
+	)
+)
--- fizzbuzz/test/fizzbuzz/core_test.clj
+++ fizzbuzz/test/fizzbuzz/core_test.clj
@@ -3,10 +3,42 @@
             [fizzbuzz.core :refer :all]))
 
 
-(deftest is-divisible-by-three
+(deftest three-is-divisible-by-three
 	(testing "returns true for 3")
 		(is (= true (divisible-by-three? 3))))
 
-(deftest is-divisible-by-three
+(deftest one-is-not-divisible-by-three
 	(testing "returns false for 1")
 		(is (= false (divisible-by-three? 1))))
+
+(deftest five-is-divisible-by-five
+	(testing "returns true for 5")
+		(is (= true (divisible-by-five? 5))))
+
+(deftest one-is-not-divisible-by-five
+	(testing "returns false for 1")
+		(is (= false (divisible-by-five? 1))))
+
+(deftest fifteen-is-divisible-by-fifteen
+	(testing "returns true for 15")
+		(is (= true (divisible-by-fifteen? 15))))
+
+(deftest twelve-is-not-divisible-by-fifteen
+	(testing "returns false for 12")
+		(is (= false (divisible-by-fifteen? 12))))
+
+(deftest game-returns-Fizz-if-divisble-by-three 
+	(testing "returns Fizz for 3")
+		(is (= "Fizz" (game 3))))
+
+(deftest game-returns-Buzz-if-divisble-by-five 
+	(testing "returns Buzz for 5")
+		(is (= "Buzz" (game 5))))
+
+(deftest game-returns-FizzBuzz-if-divisble-by-fifteen
+	(testing "returns FizzBuzz for 15")
+		(is (= "FizzBuzz" (game 15))))
+
+(deftest game-returns-number-if-not-divisible-by-fife-or-three
+	(testing "returns 13 for 13")
+		(is (= 13 (game 13))))
